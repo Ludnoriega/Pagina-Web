23968f91b9c60dfa4e0b9e0ade398be41bafc95a
Ludnoriega <noriegaludmila3@gmail.com>
2022-09-12 00:09:52 -0300

    ultimos cambios

--- LOGIN/database/base de datos Plataforma.sql
+++ LOGIN/database/base de datos Plataforma.sql
@@ -9,13 +9,21 @@ contrasenia VARCHAR(10),
 rol VARCHAR(10),
 PRIMARY KEY (id));
 
+CREATE TABLE curso(
+cod_curso VARCHAR(5),
+año INT,
+division varchar (1),
+Primary key (cod_curso));
+
 CREATE TABLE Alumno(
-DNI_alumno int NOT NULL,
+DNI_alumno int,
 nombre_alumno varchar (15),
 apellido_alumno varchar (15),
 contenidos varchar (300),
 id INT,
+ID_CURSO VARCHAR(5),
 Primary key(DNI_alumno),
+FOREIGN KEY (ID_CURSO) REFERENCES curso(cod_curso),
 FOREIGN KEY (id) REFERENCES usuarios (id));
 
 CREATE TABLE Profesor(
@@ -27,19 +35,10 @@ Primary key (DNI),
 FOREIGN KEY (id) REFERENCES usuarios (id)
 );
 
-CREATE TABLE curso(
-cod_curso INT NOT NULL,
-año INT,
-division varchar (1),
-DNI_alumno INT,
-Primary key (cod_curso),
-Foreign Key (DNI_alumno) REFERENCES alumno(DNI_alumno));
-
-
 CREATE TABLE materia(
 cod_materia VARCHAR (25) NOT NULL,
 nombre varchar (25),
-cursos INT,
+cursos VARCHAR(5),
 Primary key (cod_materia),
 Foreign Key (cursos) REFERENCES Curso(cod_curso));
 
@@ -61,20 +60,37 @@ INSERT INTO usuarios (usuarioo, contrasenia, rol) VALUES
 ("ANA", "54321", "alumno"),
 ("LUDMI", "121103", "alumno");
 
-INSERT INTO alumno (DNI_alumno, nombre_alumno, apellido_alumno, contenidos, id) VALUES 
-(46453515, "Ana Luz", "Bazan", "Debe: Proyecto de CNC", 2),
-(98765432, "Anahi Ludmila", "Noriega", "Sin comentarios", 3);
+INSERT INTO curso (cod_curso, año, division) VALUES 
+("7A", 7, "A"),
+("6A", 6, "A"),
+("6B", 6, "B"),
+("6C", 6, "C"),
+("5A", 5, "A"),
+("5B", 5, "B"),
+("5C", 5, "C"),
+("4A", 4, "A"),
+("4B", 4, "B"),
+("4C", 4, "C"),
+("3A", 3, "A"),
+("3B", 3, "B"),
+("3C", 3, "C"),
+("2A", 2, "A"),
+("2B", 2, "B"),
+("2C", 2, "C"),
+("1A", 1, "A"),
+("1B", 1, "B"),
+("1C", 1, "C");
+
+INSERT INTO alumno (DNI_alumno, nombre_alumno, apellido_alumno, contenidos, id, ID_CURSO) VALUES 
+(46453515, "Ana Luz", "Bazan", "Debe: Proyecto de CNC", 2, "6A"),
+(98765432, "Anahi Ludmila", "Noriega", "Sin comentarios", 3, "2C");
 
 
 INSERT INTO profesor (DNI, nombre, apellido, id) VALUES 
 (45485190, "Ludmila Anahí", "Noriega", 1);
 
-INSERT INTO curso (cod_curso, año, division, DNI_alumno) VALUES 
-(7, 7, "A", 46453515),
-(6, 6, "A", 98765432);
-
 INSERT INTO materia (cod_materia, nombre, cursos) VALUES 
-("PROGRAMACION","Programación", 7);
+("PROGRAMACION","Programación", "7A");
 
 INSERT INTO nota (materiia, alumnoo, numero) VALUES 
 ("PROGRAMACION", 46453515, 9);
